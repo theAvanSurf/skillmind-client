852b1e8f3ba6170f117b29bf17f83edd5ec06579
elvinelia <eliaslugo253@gmail.com>
2026-01-28 11:17:29 -0400

    Arreglado RegisterPage y RegisterForm, eliminado react-dom

--- src/app/(guest)/register/page.tsx
+++ src/app/(guest)/register/page.tsx
@@ -1,10 +1,11 @@
+"use client";
+
 import RegisterForm from "src/components/forms/RegisterForm";
 
 export default function RegisterPage() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex min-h-screen items-center justify-center bg-gradient-to-b from-white via-orange-50 to-orange-400">
       <RegisterForm />
     </main>
   );
-  
 }
